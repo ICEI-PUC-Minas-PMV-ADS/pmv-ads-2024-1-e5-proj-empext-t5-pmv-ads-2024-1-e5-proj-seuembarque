92b6468ca5b96a394e6705e6c9a74ff5f09e1c84
GuruDevOp7884 <gugaalves92@hotmail.com>
2024-03-12 19:14:51 -0300

    Codigio fonte C#

---
+++ codigo-fonte/SeuEmbarque/src/Puc.SeuEmbarque.Infra.ApiData/Interface/ISkyscannerRepository.cs
@@ -0,0 +1,14 @@
+﻿using Puc.SeuEmbarque.Domain.Models.Skyscanner;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Puc.SeuEmbarque.Infra.ApiData.Interface
+{
+    public interface ISkyscannerRepository
+    {
+        Task<List<Aeroporto>> ListarAeroportos(string termo);
+    }
+}
---
+++ codigo-fonte/SeuEmbarque/src/Puc.SeuEmbarque.Services/Interface/IAeroportoService.cs
@@ -0,0 +1,14 @@
+﻿using Puc.SeuEmbarque.Domain.Models.Skyscanner;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Puc.SeuEmbarque.Services.Interface
+{
+    public interface IAeroportoService
+    {
+        Task<List<AeroportoDto>> GetAeroportos(string termo);
+    }
+}
