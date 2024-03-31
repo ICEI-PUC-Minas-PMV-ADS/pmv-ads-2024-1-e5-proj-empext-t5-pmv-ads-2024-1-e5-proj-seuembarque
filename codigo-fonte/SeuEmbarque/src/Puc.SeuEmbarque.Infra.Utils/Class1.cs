edad2c0c6f64c672eed024f0546bc451ebaed830
Bruno Ferreira <bruno.ferreira@efeso.com>
2024-03-31 10:19:47 -0300

    AJuste

--- codigo-fonte/SeuEmbarque/src/Puc.SeuEmbarque.Infra.Utils/Class1.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace Puc.SeuEmbarque.Infra.Utils
-{
-    public class Class1
-    {
-
-    }
-}
--- codigo-fonte/SeuEmbarque/src/Puc.SeuEmbarque.Presentation/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace Puc.SeuEmbarque.Presentation.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
